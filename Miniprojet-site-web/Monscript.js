8446f431ee3ff20557c25d2b65e2717f7c43880d
ahmedaithssain <122237008+ahmedaithssain@users.noreply.github.com>
2023-01-08 23:53:15 +0100

    Add files via upload

---
+++ Miniprojet-site-web/Monscript.js
@@ -0,0 +1,36 @@
+/*var compteurImage=1;
+var totalimage=10;
+
+function slider(x){
+    var image=document.getElementById('img');
+    compteurImage=compteurImage + x;
+    image.src="images/corona"+compteurImage+".jpg";
+
+    if(compteurImage>=totalimage)
+      {
+        compteurImage=1;
+      }
+    
+    if(compteurImage<1)
+      {
+        compteurImage=totalimage;
+      }
+}
+
+function sliderAuto(){
+    var image=document.getElementById('img');
+    compteurImage=compteurImage + 1;
+    image.src="images/corona"+compteurImage+".jpg";
+
+    if(compteurImage>=totalimage)
+      {
+        compteurImage=1;
+      }
+    
+    if(compteurImage<1)
+      {
+        compteurImage=totalimage;
+      }
+}
+
+window.setInterval(sliderAuto,3000);*/
